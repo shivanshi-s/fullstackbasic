95c39fe2857b13dec80e25133303fd7828be3292
shivanshi <shivanshi.saxena2@gmail.com>
2024-01-02 21:55:27 +0530

    basics of frontend and backend

---
+++ backend/server.js
@@ -0,0 +1,43 @@
+import express  from "express"; //module js syntax - works asynchronously, to change error-> package -> module assembling
+const app = express();
+
+// app.get('/',  (req, res) => {
+//     res.send("Server is ready");
+// });
+
+//get a list of 5 jokes
+app.get('/jokes', (req, res) => {
+    const jokes = [
+        {
+            id : 1,
+            title : 'A joke',
+            content : 'This is a joke'
+        },
+        {
+            id : 2,
+            title : '2nd joke',
+            content : 'This is a 2 joke'
+        },
+        {
+            id : 3,
+            title : '3rd joke',
+            content : 'This is a 3 joke'
+        },
+        {
+            id : 4,
+            title : '4th joke',
+            content : 'This is a 4 joke'
+        },
+        {
+            id : 5,
+            title : '5th joke',
+            content : 'This is a 5 joke'
+        }
+    ]
+    res.send(jokes);
+})
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server at http://localhost:${port}`);
+});
