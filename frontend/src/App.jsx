505978417992d7cfb091a50bcf7ac8111e0db5aa
shivanshi <shivanshi.saxena2@gmail.com>
2024-01-04 01:03:27 +0530

    connecting

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,20 @@
 
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
+import axios from 'axios'
 
 function App() {
   const [jokes, setJokes] = useState([])
+
+  useEffect(() => {
+    axios.get('/api/jokes')
+      .then((response) => {
+        setJokes(response.data)
+      })
+      .catch((error) =>{
+        console.log(error)
+      })
+  })
 
   return (
     <>
@@ -11,12 +22,12 @@ function App() {
       <p>JOKES : {jokes.length}</p>
 
       {
-        jokes.map((joke, index) => {
+        jokes.map((joke) => (
           <div key={joke.id}>
             <h3>{joke.title}</h3>
             <p>{joke.content}</p>
           </div>
-        })
+        ))
       }
     </>
   )
